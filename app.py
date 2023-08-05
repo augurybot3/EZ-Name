57a5f3d88d8e5816094d8c9dd3a05b2942d43e6d
augurybot3 <augurybot@gmail.com>
2023-08-05 17:41:02 -0400

    include file dialog and message box for better user experience

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
+import tkinter as tk
+from tkinter import filedialog,  messagebox
 
 def rename_files():
-    directory = input("Please enter the directory path: ")
-    rename_all = input("Do you want to rename files in subdirectories as well? Type 'RENAME-ALL-FILES' to confirm: ")
-    if rename_all == 'RENAME-ALL-FILES':
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    directory = filedialog.askdirectory()
+    rename_all = messagebox.askyesno("Rename All Files", "Do you want to rename files in subdirectories as well?")
+    if rename_all:
         for dirpath, dirnames, filenames in os.walk(directory):
             rename_in_directory(dirpath, filenames)
     else:
@@ -12,22 +16,25 @@ def rename_files():
 
 def rename_in_directory(directory, filenames):
     for filename in filenames:
+        old_file_path = os.path.join(directory, filename)
+        if not os.path.isfile(old_file_path):
+            continue
         if ' ' not in filename and '_' not in filename:
             print(f"Skipped filename: {filename}")
             continue  # Skip files without spaces or underscores
         new_filename = filename.replace(' ', '-').replace('_', '-')
-        if new_filename in filenames:
+        new_file_path = os.path.join(directory, new_filename)
+        if os.path.exists(new_file_path):
             base, extension = os.path.splitext(new_filename)
-            i = 1
-            while new_filename in filenames:
+            i  = 1
+            while os.path.exists(new_file_path):
                 new_filename = f"{base}-copy{i}{extension}"
+                new_file_path = os.path.join(directory, new_filename)
                 i += 1
             print(f"Filename conflict: {base + extension} already exists. Renaming conflicted file to {new_filename}")
         else:
             print(f"Renamed {filename} to {new_filename}")
-        source = os.path.join(directory, filename)
-        destination = os.path.join(directory, new_filename)
-        os.rename(source, destination)  # Rename the file
+        os.rename(old_file_path, new_file_path)
 
 # Usage
 rename_files()
